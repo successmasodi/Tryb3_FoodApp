da7117eb60ba7a4911dfcdbbc847690328889fe5
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-25 09:35:59 +0100

    add initial Django project structure with ASGI, WSGI, settings, and URL configurations

---
+++ config/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SkyLine API",
+        default_version="v1",
+        description="""
+            An API for booking flights.
+            
+            **Servers:**
+            - Local: [http://localhost:8000](http://localhost:8000)
+            - Production: [https://domain.com/](https://domain.com/)
+            """,
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="suskidee@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("auth/", include("apps.authorization.urls")),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
