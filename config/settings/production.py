f753283344a55e4e45cb1213f645c638645f541f
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-05-15 18:01:54 +0100

    add closing curyl brac

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,7 @@ DATABASES = {
         
     }
 }
-
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -20,7 +20,6 @@ DATABASES = {
             'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
         },
-
     }
 }
 
